2b379cd43c3b42c081c62fe577eba868f2173677
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-23 08:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-var isDate = function (input) {
-  //   write your code here
+var isDate = function (date) {
+  if (typeof date === 'object' && date instanceof Date) {
+    return !isNaN(date.getTime());
+  } else if (typeof date === 'string' || typeof date === 'number') {
+    return !isNaN(Date.parse(date));
+  } else {
+    return false;
+  }
 };
-
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
+
